4f70596837f32056a0abb134c4fbfff9ae8d210d
virendra-agarwal <90118724+virendra-agarwal@users.noreply.github.com>
2022-01-18 15:46:16 -0500

    Update main.tf

--- code/terraform/04-terraform-module/module-example/modules/services/webserver-cluster/main.tf
+++ code/terraform/04-terraform-module/module-example/modules/services/webserver-cluster/main.tf
@@ -144,7 +144,7 @@ data "terraform_remote_state" "db" {
   config = {
     bucket = var.db_remote_state_bucket
     key    = var.db_remote_state_key
-    region = "us-east-2"
+    region = "us-east-1"
   }
 }
 
